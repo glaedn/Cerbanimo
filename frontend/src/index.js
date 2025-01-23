6ad0b9d53da135f0f202851bda95e34a9d5dcec1
Glaedn <glaedn@gmail.com>
2025-01-23 11:05:22 -0500

    fleshing out Auth0 authentification route

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,26 @@
-import { Auth0Provider } from '@auth0/auth0-react';
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
+import { Auth0Provider } from '@auth0/auth0-react';
+import './index.css';
 import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <Auth0Provider
-    domain="YOUR_AUTH0_DOMAIN"
-    clientId="YOUR_CLIENT_ID"
-    authorizationParams={{
-      redirect_uri: window.location.origin,
-    }}
-  >
-    <App />
-  </Auth0Provider>,
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <Auth0Provider
+      domain="dev-i5331ndl5kxve1hd.us.auth0.com"
+      clientId="vh3gl8nk3NF6uNkjRT8suuzfFjgCIdiB"
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+    >
+      <App />
+    </Auth0Provider>,
   document.getElementById('root')
+
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
