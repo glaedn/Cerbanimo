4e1db1c318aaf0074aa8ddf422e4099151aac860
Glaedn <glaedn@gmail.com>
2025-12-21 01:04:51 -0500

    fixed errors related to previous project

--- src/App.jsx
+++ src/App.jsx
@@ -3,32 +3,32 @@ import * as React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import PrivateRoute from "./components/PrivateRoute.jsx";
-import Orbit from "./pages/Orbit.jsx";
+//import Orbit from "./pages/Orbit.jsx";
 import ProfilePage from "./pages/ProfilePage/ProfilePage.jsx";
-import IntentionPages from "./pages/IntentionPages.jsx";
+//import IntentionPages from "./pages/IntentionPages.jsx";
 import SiteNav from "./pages/SiteNav.jsx";
-import IntentionCreation from "./pages/IntentionCreation.jsx";
-import Intention from "./pages/Intention.jsx";
-import CapabilityTree from "./pages/CapabilityTree.jsx";
+//import IntentionCreation from "./pages/IntentionCreation.jsx";
+//import Intention from "./pages/Intention.jsx";
+//import CapabilityTree from "./pages/CapabilityTree.jsx";
 import RewardDashboard from "./pages/RewardDashboard.jsx";
 import PublicProfile from "./pages/PublicProfile.jsx";
 import BadgeCreation from "./pages/BadgeCreation.jsx";
 import HomePage from "./pages/HomePage.jsx";
-import IntentionLotusMap from "./pages/IntentionLotusMap.jsx";
-import RealmCreation from "./pages/RealmCreation.jsx";
-import RealmHub from "./pages/RealmHub.jsx";
-import Realms from "./pages/Realms.jsx";
+//import IntentionLotusMap from "./pages/IntentionLotusMap.jsx";
+//import RealmCreation from "./pages/RealmCreation.jsx";
+//import RealmHub from "./pages/RealmHub.jsx";
+//import Realms from "./pages/Realms.jsx";
 import UserPortfolio from "./pages/UserPortfolio.jsx";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import GalacticActivityMap from "./components/GalacticActivityMap/GalacticActivityMap.jsx";
 import OnboardingPage from "./pages/OnboardingPage/OnboardingPage";
 import WaitingListPage from "./pages/WaitingListPage.jsx"; // Added import
-import CallTheCosmos from "./pages/CallTheCosmos.jsx";
+//import CallTheCosmos from "./pages/CallTheCosmos.jsx";
 import AuthWrapper from "./AuthWrapper.jsx";
-import RealmsMesh from "./pages/RealmsMesh.jsx";
-import ManifestationSession from "./pages/ManifestationSession.jsx";
-import AnalyticsDashboard from "./pages/AnalyticsDashboard.jsx";
+//import RealmsMesh from "./pages/RealmsMesh.jsx";
+//import ManifestationSession from "./pages/ManifestationSession.jsx";
+//import AnalyticsDashboard from "./pages/AnalyticsDashboard.jsx";
 import Rezzler from "./mobile/Rezzler.jsx";
 
 const App = () => {
@@ -51,14 +51,6 @@ const App = () => {
             }
           />
           <Route
-            path="/orbit"
-            element={
-              <PrivateRoute>
-                <Orbit />
-              </PrivateRoute>
-            }
-          />
-          <Route
             path="/reward-dashboard"
             element={
               <PrivateRoute>
@@ -75,83 +67,10 @@ const App = () => {
             }
           />
           <Route
-            path="/intentions"
-            element={
-              <PrivateRoute>
-                <IntentionPages />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/declare-intention"
-            element={
-              <PrivateRoute>
-                <IntentionCreation />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/intention/:intentionId"
-            element={
-              <PrivateRoute>
-                <Intention />
-              </PrivateRoute>
-            }
-          />
-          <Route path="/profile/public/:userId" element={<PublicProfile />} />
-          <Route
-            path="/profile/capability-tree"
-            element={
-              <PrivateRoute>
-                <CapabilityTree />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/form-new-realm"
-            element={
-              <PrivateRoute>
-                <RealmCreation />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/realm/:realmId"
-            element={
-              <PrivateRoute>
-                <RealmHub />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/realms"
-            element={
-              <PrivateRoute>
-                <Realms />
-              </PrivateRoute>
-            }
-          />
-          <Route
             path="/BadgeCreation"
             element={
               <PrivateRoute>
                 <BadgeCreation />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/lotus-map/:intentionId"
-            element={
-              <PrivateRoute>
-                <IntentionLotusMap />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/lotus-map/:intentionId/:petalId"
-            element={
-              <PrivateRoute>
-                <IntentionLotusMap />
               </PrivateRoute>
             }
           />
@@ -171,38 +90,6 @@ const App = () => {
             element={
               <PrivateRoute>
                 <GalacticActivityMap />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/call-the-cosmos"
-            element={
-              <PrivateRoute>
-                <CallTheCosmos />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/realms-mesh"
-            element={
-              <PrivateRoute>
-                <RealmsMesh />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/manifestation-session"
-            element={
-              <PrivateRoute>
-                <ManifestationSession />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/analytics-dashboard"
-            element={
-              <PrivateRoute>
-                <AnalyticsDashboard />
               </PrivateRoute>
             }
           />
