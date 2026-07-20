b49bb00334fa5475bf8fb39859431c7b49a80abe
Glaedn <glaedn@gmail.com>
2026-07-20 14:32:37 -0400

    Fix Linux stylesheet resolution

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,6 @@
 import { motion } from "framer-motion";
 import { useNavigate } from "react-router-dom";
-import "./Homepage.css";
+import "./HomePage.css";
 
 export default function Homepage() {
   // Get the navigate function from React Router
